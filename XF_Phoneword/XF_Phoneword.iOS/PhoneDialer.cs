800d3dceb78edb8091a94d812be8a1de465c5371
s_watanabe <s_watanabe@dialonet.co.jp>
2017-10-25 21:06:33 +0900

    work in progress

---
+++ XF_Phoneword/XF_Phoneword.iOS/PhoneDialer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using XF_Phoneword.iOS;
+
+[assembly: Dependency(typeof(PhoneDialer))]
+
+namespace XF_Phoneword.iOS
+{
+    public class PhoneDialer : IDialer
+    {
+        public bool Dial(string number)
+        {
+            return UIApplication.SharedApplication.OpenUrl(
+                new NSUrl("tel:" + number));
+        }
+    }
+}
---
+++ XF_Phoneword/XF_Phoneword/MainPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XF_Phoneword
+{
+    public partial class MainPage : ContentPage
+    {
+        string translatedNumber;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        void OnTranslate(object sender, EventArgs e)
+        {
+            translatedNumber = Core.PhoneTranslator.ToNumber(phoneNumberText.Text); 
+            if (!string.IsNullOrWhiteSpace(translatedNumber))
+            {
+                callButton.IsEnabled = true;
+                callButton.Text = "Call " + translatedNumber;
+            }
+            else
+            {
+                callButton.IsEnabled = false;
+                callButton.Text = "Call";
+            }
+        }
+
+        async void OnCall(object sender, EventArgs e)
+        {
+            var call = await DisplayAlert(
+                "Dial a Number",
+                "Would you like to call " + translatedNumber + "?",
+                "Yes",
+                "No");
+            if (call)
+            {
+                try
+                {
+                    var status = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Phone);
+                    if (status != PermissionStatus.Granted)
+                    {
+                        if(await CrossPermissions.Current.ShouldShowRequestPermissionRationaleAsync(Permission.Phone))
+                        {
+                            await DisplayAlert("Need Permission", "It will get permission of phonecall", "OK");
+                        }
+                        var results = await CrossPermissions.Current.RequestPermissionsAsync(new[] { Permission.Phone });
+                        status = results[Permission.Phone];
+                    }
+                    if(status == PermissionStatus.Granted)
+                    {
+                        var dialer = DependencyService.Get<IDialer>();
+                        if (dialer != null)
+                            dialer.Dial(translatedNumber);
+                    }
+                    else if(status != PermissionStatus.Unknown)
+                    {
+                        await DisplayAlert("Permission Denied", "Can not continue, try again.", "OK");
+                    }
+
+                }
+                catch(Exception ex)
+                {
+                    System.Diagnostics.Debug.WriteLine(ex.InnerException);
+                }
+                
+            }
+        }
+    }
+}
